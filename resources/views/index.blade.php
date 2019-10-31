23a55f74816efc60341e8f535cf82b1108aca8a5
xinru <s3a632065@student.ncut.edu.tw>
2019-11-01 04:23:12 +0800

    7. 設定將超連結的標籤<a>，更改為對應的 route 名稱
    navigation.blade.php  &  index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,7 +25,8 @@
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <div class="post-preview">
-                    <a href="post.html">
+                    <!--<a href="post.html">-->
+                    <a href="{{route('posts.show')}}">
                         <h2 class="post-title">
                             Man must explore, and this is exploration at its greatest
                         </h2>
@@ -37,7 +38,8 @@
                 </div>
                 <hr>
                 <div class="post-preview">
-                    <a href="post.html">
+                    <!--<a href="post.html">-->
+                    <a href="{{route('posts.show')}}">
                         <h2 class="post-title">
                             I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
                         </h2>
@@ -46,7 +48,8 @@
                 </div>
                 <hr>
                 <div class="post-preview">
-                    <a href="post.html">
+                    <!--<a href="post.html">-->
+                    <a href="{{route('posts.show')}}">
                         <h2 class="post-title">
                             Science has not yet mastered prophecy
                         </h2>
@@ -58,7 +61,8 @@
                 </div>
                 <hr>
                 <div class="post-preview">
-                    <a href="post.html">
+                    <!--<a href="post.html">-->
+                    <a href="{{route('posts.show')}}">
                         <h2 class="post-title">
                             Failure is not an option
                         </h2>
