a19d3543086608a5d6e1339a654a0b72a32cf882
xinru <s3a632065@student.ncut.edu.tw>
2019-11-06 11:11:02 +0800

    練習2-1 新增資料 (使用 create方法)

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,10 @@ Route::get('contact', ['as' => 'posts.contact', 'uses' => 'PostsController@conta
 
 Route::get('/', function () {
     return view('welcome');
+
+    \App\Post::create([
+        'title'=>'test title',
+        'content'=>'test content',
+    ]);
+
 });
