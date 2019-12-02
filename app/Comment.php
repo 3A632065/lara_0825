20f67effc293cf6ada1adf8188fa6eb46d83e0c8
xinru <s3a632065@student.ncut.edu.tw>
2019-12-03 00:43:59 +0800

    練習7-3 新增關聯
    開啟 Comment.php

--- app/Comment.php
+++ app/Comment.php
@@ -3,10 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Post;
 
 class Comment extends Model
 {
     //
     protected $fillable = ['content'];
+
+    //增加關聯
+    public function post()
+    {
+        return $this ->belongsTo(Post::class);
+    }
 
 }
